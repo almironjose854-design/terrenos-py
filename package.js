f68077da26456669c63b4f33229b0c4cab776887
almironjose854-design <almironjose854@gmail.com>
2026-01-20 11:06:09 -0300

    Add files via upload

---
+++ package.js
@@ -0,0 +1,16 @@
+{
+  "name": "terrenos-py",
+  "version": "1.0.0",
+  "description": "Plataforma web de terrenos con panel de administración",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "engines": {
+    "node": ">=18"
+  },
+  "dependencies": {
+    "express": "^4.19.2",
+    "cors": "^2.8.5"
+  }
+}
